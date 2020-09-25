05d2ef2c22c12939faba4f6e6dec8e321ca0fdd5
kumarmadisetty <kumarmadisetty17@gmail.com>
2020-09-25 13:46:11 +0530

    second commit

---
+++ src/main/java/com/example/springbootjenkinspipeline/SpringBootJenkinsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjenkinspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsPipelineApplication.class, args);
+	}
+
+}
